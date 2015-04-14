bb04630c043625c3e350e26bd0f21b75a5a654ca
lyan <lyan@lyan-iMac.AirDream>
2015-04-14 14:07:39 +0800

    Initial Commit

---
+++ 环信/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  环信
+//
+//  Created by lyan on 15/4/14.
+//  Copyright (c) 2015年 com.longjianchuanmei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
